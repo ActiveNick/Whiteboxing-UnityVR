a1f7f3380151b0f9e4d12734dcfab9538194b4f4
Nick Landry <nick.landry@microsoft.com>
2018-04-05 16:16:34 -0400

    Can spawn balls + restricted button selection to controller trigger

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -11,36 +11,44 @@ public class ButtonHandler : InteractionReceiver {
 
     public float spawnDistance = 0.3f;
     public GameObject prefabCube;
+    public GameObject prefabSphere;
 
     protected override void InputDown(GameObject obj, InputEventData eventData)
     {
         Debug.Log(obj.name + " : InputDown");
 
-        Ray aimingRay;
-        Vector3 spawnPoint = Vector3.zero;
-        if (eventData.InputSource.TryGetPointingRay(eventData.SourceId, out aimingRay))
+        if (eventData.PressType == InteractionSourcePressInfo.Select)
         {
-            spawnPoint = aimingRay.origin + (Vector3.Normalize(aimingRay.direction) * spawnDistance);
-        }
+            Ray aimingRay;
+            Vector3 spawnPoint = Vector3.zero;
+            if (eventData.InputSource.TryGetPointingRay(eventData.SourceId, out aimingRay))
+            {
+                spawnPoint = aimingRay.origin + (Vector3.Normalize(aimingRay.direction) * spawnDistance);
+            }
 
-        switch (obj.name)
-        {
-            case "HoloButton-Cube":
-                SpawnObject(prefabCube, spawnPoint);
-                break;
-            case "HoloButton-Sphere":
-                break;
-            case "HoloButton-Cylinder":
-                break;
-            case "HoloButton-Reset":
-                break;
-            default:
-                break;
+            switch (obj.name)
+            {
+                case "HoloButton-Cube":
+                    SpawnObject(prefabCube, spawnPoint);
+                    break;
+                case "HoloButton-Sphere":
+                    SpawnObject(prefabSphere, spawnPoint);
+                    break;
+                case "HoloButton-Cylinder":
+                    break;
+                case "HoloButton-Reset":
+                    break;
+                default:
+                    break;
+            } 
         }
     }
 
     private void SpawnObject(GameObject prefab, Vector3 location)
     {
-        Rigidbody newObj = Instantiate(prefab, location, prefab.transform.rotation).GetComponent<Rigidbody>();
+        if (prefab != null)
+        {
+            Rigidbody newObj = Instantiate(prefab, location, prefab.transform.rotation).GetComponent<Rigidbody>();
+        }
     }
 }
